17ca5f38237ae987b40c77476871f04eeeab311a
E <72871814+Emily0Rose@users.noreply.github.com>
2022-08-17 01:04:05 +0800

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        GoodStudent st1 = new GoodStudent();
-        BadStudent st2 = new BadStudent();
-
-        st1.study();
-        st2.study();
-    }
-}
