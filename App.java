bf79d0a91dcb7cee00978250e538926425dae661
E <72871814+Emily0Rose@users.noreply.github.com>
2022-08-24 11:07:02 +0800

    parimophism test

---
+++ App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Animal myAnimal = new Animal();
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+
+        myAnimal.sound();
+        myDog.sound();
+        myCat.sound();
+    }
+}
