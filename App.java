45fa8ec53350d23ad8b120f0c3fb66ac1109b46b
E <72871814+Emily0Rose@users.noreply.github.com>
2022-08-24 11:35:53 +0800

    Inner class test
    there are also System.out.println mini test

--- App.java
+++ App.java
@@ -1,11 +1,17 @@
+class OuterClass {
+    int x = 2;
+
+    class InnerClass {
+        int y = 9;
+    }
+}
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Animal myAnimal = new Animal();
-        Animal myDog = new Dog();
-        Animal myCat = new Cat();
-
-        myAnimal.sound();
-        myDog.sound();
-        myCat.sound();
+        OuterClass myOuter = new OuterClass();
+        OuterClass.InnerClass myInner = myOuter.new InnerClass();
+        System.out.println("myOuter.x: " + myOuter.x + " myInner.y: " + myInner.y);
+        System.out.println("x + y = " + myOuter.x + myInner.y); // 29
+        System.out.println("x + y = " + (myOuter.x + myInner.y)); // 11
     }
 }
